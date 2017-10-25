68923a6acf99b4ef1520e88076249ac29c44fbab
ankur <ankur.patankar@nykaa.com>
2017-10-25 23:04:29 +0530

    ms

--- ms/src/components/containers/ListingContainer.js
+++ ms/src/components/containers/ListingContainer.js
@@ -6,18 +6,29 @@ class ListingContainer extends Component {
     constructor() {
         super();
     }
-
+    /**
+     * Shuffles array in place. ES6 version
+     * @param {Array} a items An array containing the items.
+     */
+    shuffle = (a) => {
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+        }
+    }
     getListings = () => {
         var listings;
         if (this.props.carlist) {
             if (this.props.carlist.length) {
+                this.shuffle(this.props.carlist);
+
                 listings = this.props.carlist.map((item, index) => {
                     return <Listing key={index} item={item}/>;
                 });
             } else {
                 listings = <div><p className="no-results-p1"> No Results Found</p><p className="no-results-p2">Need a reroute! Relax your preferences to see more cars.</p> </div>;
             }
-        }else{
+        } else {
             listings = "";
         }
         return listings;
